5ff684003bca29da667d7e66e4c3fd9ff761ecd7
Aaron Bernard <reachaaronbernard@gmail.com>
2026-07-01 00:54:44 -0600

    changed nvim colorscheme; organized the plugins into 2 files (UI-plugins; Backend-plugins)

---
+++ .configs/.config/nvim/lua/plugins/backend_internal_plugins.lua
@@ -0,0 +1,88 @@
+return {
+-- Note: have to add {} to each separate plugin if in ONE plugin file 
+
+
+{
+  -- neo-tree
+
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+		
+
+		config = function()
+			-- Neo-tree config
+			require('neo-tree').setup({
+				-- options go here
+			})
+			vim.keymap.set('n', '<leader>o', ':Neotree filesystem reveal left<CR>', {}) -- changed '<leader>n' to '<C-n>' 
+
+		end
+  }
+
+  , -- comma
+
+
+
+  {
+-- telescope
+
+		'nvim-telescope/telescope.nvim', version = '*',
+		dependencies = {
+				'nvim-lua/plenary.nvim',
+				-- optional but recommended
+				{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+		},
+
+		config = function()
+		  local builtin = require('telescope.builtin')
+		  vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+		  vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+		  vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+		  vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+		end
+-- telescope
+  }
+
+
+  , -- comma
+
+
+
+
+
+  {
+-- tree-sitter
+		
+		'nvim-treesitter/nvim-treesitter',
+		lazy = false,
+		build = ':TSUpdate',
+		
+
+		config = function()
+			require('nvim-treesitter').setup {
+				-- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+				install_dir = vim.fn.stdpath('data') .. '/site'
+			}
+			
+			require('nvim-treesitter').install { 'c', 'lua', 'java', 'python', 'swift' }
+
+			vim.api.nvim_create_autocmd('FileType', {
+				pattern = { '<filetype>' },
+				callback = function() vim.treesitter.start() end,
+			})
+
+			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+		end
+-- treesitter
+  }
+
+
+
+}
---
+++ .configs/.config/nvim/lua/plugins/frontend_ui_plugins.lua
@@ -0,0 +1,70 @@
+return {
+
+-- Note: have to add {} to each separate plugin if in ONE plugin file 
+
+
+
+
+	-- colorschemes (oldworld; oxocarbon)
+		
+  --{	
+  -- oldworld colorscheme
+  	--"dgox16/oldworld.nvim", 
+		--lazy = false, 
+	  --priority = 1000,
+
+    --config = function()
+		  -- colorscheme
+			--vim.cmd.colorscheme("oldworld") -- calling the colorscheme of choice to "work"
+			--
+		--end,
+  --oldworld
+--}
+
+
+
+{ 
+  -- oxocarbon colorscheme - https://github.com/dgox16/oldworld.nvim 
+  "nyoom-engineering/oxocarbon.nvim", 
+  lazy = false, 
+  priority = 1000, 
+  config = function() 
+    vim.opt.background = "dark" -- set this to dark or light 
+    vim.cmd("colorscheme oxocarbon") end 
+  -- oxocarbon 
+} 
+
+
+
+, -- comma separating the coloschemes (above) to the lualine (below) 
+
+
+
+
+{ -- lualine
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+  config = function()
+    require('lualine').setup({
+    options = {
+      -- theme = 'dracula' -- 'normal' mode is in purple
+      -- theme = 'modus-vivendi' -- 'normal' mode is in blue
+      theme = 'horizon' -- 'normal' mode is in pinkish-red
+          
+      -- theme = 'cosmicink'
+      }, 
+        
+      sections = {
+		    lualine_x = { "encoding", { "fileformat", symbols = { unix = "" } }, "filetype" },
+	    },
+
+    })
+
+  end
+}
+
+
+}
+
+
